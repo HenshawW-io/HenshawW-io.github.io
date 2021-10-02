1ee4bea0879d5e401920b69e7fd143320a36155b
HenshawW-io <henshaw.billy@outlook.com>
2021-10-02 16:23:28 -0400

    Defined a function for the welcome message.

--- javascript/external.js
+++ javascript/external.js
@@ -41,6 +41,20 @@
 // }
 
 // arr = ['Joe', 'April', 'Mark', 'Sara'];
-uname = window.prompt("What's your name?");
+// uname = window.prompt("What's your name?");
 
-document.write('<h1>Hello. Welcome to my webpage, ' + uname + '!</h1>');
+// document.write('<h1>Hello. Welcome to my webpage, ' + uname + '!</h1>');
+
+function welcome() {
+    let a = 'Please enter your name';
+    let b = 'Type your name here';
+
+    // A prompt box is used to prompt users to inpur a value before entering a page.
+    user_name = window.prompt(a, b);
+
+    message = '<h1>Hello, welcome to my webpage, ' + user_name + '!</h1>';
+
+    return message
+}
+
+document.write(welcome());
